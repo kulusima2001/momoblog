19dae5ffc17b11953514e1ffeddfb49ad05bfe81
ZHANG Zifan <1529517208@qq.com>
2026-06-03 17:59:06 +0800

    update timeline

--- src/content/timelines/timeline-01.ts
+++ src/content/timelines/timeline-01.ts
@@ -18,26 +18,91 @@ export const timeline01: TimelineData = {
     },
     {
       id: "timeline-01-03",
-      date: "Age 13",
-      title: "[Key event placeholder]",
+      date: "2003",
+      title: "黎涵开始滑冰",
       description: "[Event description placeholder]"
     },
     {
       id: "timeline-01-04",
-      date: "Age 17",
-      title: "[Key event placeholder]",
+      date: "2007",
+      title: "李理开始滑冰",
       description: "[Event description placeholder]"
     },
     {
       id: "timeline-01-05",
-      date: "2019/03/31",
-      title: "李理退役",
+      date: "2013/04/01",
+      title: "黎涵转组北京",
       description: "[Event description placeholder]"
     },
     {
       id: "timeline-01-06",
-      date: "Present",
-      title: "[Key event placeholder]",
+      date: "2013/12/10",
+      title: "黎涵JGPF第四",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-07",
+      date: "2014夏",
+      title: "黎涵李理莫斯科外训",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-08",
+      date: "2014/12/12",
+      title: "黎涵JGPF银牌\n李理JGPF第五",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-09",
+      date: "2015/03/03",
+      title: "黎涵JWC金牌\n李理JWC第六",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-10",
+      date: "2015/12/08",
+      title: "黎涵JGPF金牌\n李理JGPF银牌",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-11",
+      date: "2016/02/27",
+      title: "黎涵JWC金牌\n李理JWC铜牌",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-12",
+      date: "2016/12/14",
+      title: "李理JGPF金牌",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-13",
+      date: "2017/03/08",
+      title: "黎涵JWC银牌\n李理JWC金牌",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-14",
+      date: "2017/12/08",
+      title: "黎涵GPF金牌\n李理GPF银牌",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-15",
+      date: "2018/02/23",
+      title: "黎涵OG银牌\n李理OG金牌",
+      description: "[Event description placeholder]"
+    },{
+      id: "timeline-01-16",
+      date: "2018/03/22",
+      title: "黎涵WC金牌",
+      description: "[Event description placeholder]"
+    },
+    {
+      id: "timeline-01-17",
+      date: "2018/03/31",
+      title: "李理退役",
       description: "[Event description placeholder]"
     },
   ]
